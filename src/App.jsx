1298514f75259b4fc4bc4f47eeb0cdea09547997
gorkavillara <gorkavillara@gmail.com>
2023-09-10 20:47:37 +0200

    Tema 04 - Trabajando con Apollo Client

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-import { useQuery } from "@tanstack/react-query"
-import axios from "axios"
-import './App.css'
+import { gql, useQuery } from "@apollo/client"
+import "./App.css"
 
-const url = "https://pokeapi.co/api/v2/pokemon/charmander"
+const GET_RICK = gql`
+    query {
+        characters(page: 2, filter: { name: "rick" }) {
+            info {
+                count
+            }
+            results {
+                name
+            }
+        }
+        location(id: 1) {
+            id
+        }
+        episodesByIds(ids: [1, 2]) {
+            id
+        }
+    }
+`
 
 function App() {
-  const { isLoading, isError, error, data, status } = useQuery({
-    queryKey: ['pokemon'],
-    queryFn: () => {
-      return axios.get(url)
-    }
-  })
+    const { data, loading, error } = useQuery(GET_RICK)
+    console.log({ data, loading, error })
 
-  if (isLoading) return <h3>Loading...</h3>
-  if (isError) return <h3>Error! {error.message}</h3>
+    if (loading) return <h3>Loading...</h3>
+    if (error) return <h3>Error! {error.message}</h3>
 
-  console.log(data)
-  return (
-    <>
-      Nombre del pokemon: {data.data.name}
-    </>
-  )
+    return <>Nombre del primero: {data.characters.results[0].name}</>
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
+import "./index.css"
 
-const queryClient = new QueryClient()
+const apolloClient = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "https://rickandmortyapi.com/graphql"
+})
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <ApolloProvider client={apolloClient}>
+            <App />
+        </ApolloProvider>
+    </React.StrictMode>
 )
