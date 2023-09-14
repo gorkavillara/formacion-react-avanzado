15f022e5703941f5557959c3d2dd29d3322ce74c
gorkavillara <gorkavillara@gmail.com>
2023-09-14 17:17:09 +0200

    Tema 06 - Storybook

--- storybook-static/assets/preview-9b9ef28c.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-ea7715b4.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-6080716a.js"),["./DocsRenderer-3PUGWF3O-6080716a.js","./iframe-ea7715b4.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-19325aea.js","./index-01dccfe7.js","./index-db334f57.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-9b9ef28c.js.map
