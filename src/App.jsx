bda4a0c740a5b4e3c76c04667eb68593ef2ef83f
gorkavillara <gorkavillara@gmail.com>
2023-09-18 12:15:41 +0200

    Tema 06 - Ejercicios

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css"
 import Card from "./components/Card/Card"
+import Spinner from "./components/Spinner/Spinner"
 
 function App() {
     return (
         <div style={{ display: "grid", placeContent: "center" }}>
-            <Card
+            {/* <Card
                 textoPrimario="Primero"
                 textoSecundario="Segundo"
                 colorTexto="rgb(50, 50, 50)"
-            />
+            /> */}
+            <Spinner size="large" />
         </div>
     )
 }
