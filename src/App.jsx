3665debc9e1f37b56b56700ef61c35f105b9ca3d
gorkavillara <gorkavillara@gmail.com>
2023-09-18 10:22:22 +0200

    Tema 08 - useId

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css"
 import Formulario from "./components/Formulario"
+import Formulario_sin_uId from "./components/Formulario_sin_uId"
 
 function App() {
     return (
---
+++ src/components/Formulario_sin_uId.jsx
@@ -0,0 +1,10 @@
+const Formulario = () => {
+    return (
+        <>
+            <label htmlFor="nombre">Nombre</label>
+            <input type="text" id="nombre" />
+        </>
+    )
+}
+
+export default Formulario
