e9dc1d24bd154b957a643781b0cc2c9a64b8d90f
gorkavillara <gorkavillara@gmail.com>
2024-06-24 14:28:49 +0200

    Tema 08 - useTransition con formularios

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import { useState, useTransition } from "react"
-import Lista from "./components/Lista"
 import "./App.css"
+import Form_con_uT from "./components/Form_con_uT"
 
 function App() {
-    const [isPending, startTransition] = useTransition()
-    const [contador, setContador] = useState(0)
-    const [mostrarLista, setMostrarLista] = useState(false)
     return (
-        <div>
-            <button onClick={() => setContador((prev) => prev + 1)}>
-                {contador}
-            </button>
-            <button
-                onClick={() =>
-                    startTransition(() => setMostrarLista((prev) => !prev))
-                }
-            >
-                {mostrarLista ? "Ocultar" : "Mostrar"} lista
-            </button>
-            {mostrarLista && <Lista />}
+        <div style={{ height: "100vh", display: "grid", placeContent: "center" }}>
+            <Form_con_uT />
         </div>
     )
 }
