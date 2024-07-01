914aa5725e511e2ab4d3d45e61c2ccffeeb51930
gorkavillara <gorkavillara@gmail.com>
2024-07-01 21:15:25 +0200

    Tema 19: Custom elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css"
-import Form_con_uT from "./components/Form_con_uT"
 
 function App() {
+    const data = { name: "John Doe" }
+
     return (
-        <div style={{ height: "100vh", display: "grid", placeContent: "center" }}>
-            <Form_con_uT />
+        <div
+            style={{ height: "100vh", display: "grid", placeContent: "center" }}
+        >
+            <custom-element data={JSON.stringify(data)}></custom-element>
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,20 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 
+customElements.define(
+    "custom-element",
+    class extends HTMLElement {
+        connectedCallback() {
+            this.render()
+        }
+
+        render() {
+            const data = JSON.parse(this.getAttribute("data"))
+            this.innerHTML = `<div>${data.name}</div>`
+        }
+    }
+)
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <App />
