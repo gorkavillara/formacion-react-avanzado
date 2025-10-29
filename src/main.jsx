27598fefdd4df9a300640a0b26993910afba047c
gorkavillara <gorkavillara@gmail.com>
2025-10-29 17:49:25 +0100

    Actualización de versiones

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
 )
-
-// if ("navigator" in window) {
-//   navigator.serviceWorker.register("service-worker.js")
-// }
