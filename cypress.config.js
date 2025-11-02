45a27cf8078ce3a5f04c383a46c5d8a6334a38a5
gorkavillara <gorkavillara@gmail.com>
2025-11-02 13:12:30 +0100

    Actualizado eslint

--- cypress.config.js
+++ cypress.config.js
@@ -2,17 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) { // eslint-disable-line
+    setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "vite",
-    },
-  },
-
-  projectId: "cti1te"
 });
